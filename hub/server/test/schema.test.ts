438f46beae25c6fc06c29256297572c080b9a389
Sergey Padurets <sergey@padurets.com>
2026-09-24 09:16:10 +0000

    Version 0.3.0

--- hub/server/test/schema.test.ts
+++ hub/server/test/schema.test.ts
@@ -10,6 +10,7 @@ import {STEPS} from '../store/schema.js';
  */
 const RELEASED = [
   '02cb748c205697bd5af76c42c46d7bdb09ab4ec3d311a7841f89ccf552fb48ff', // 0.2
+  '93bef51f9d1b8ff1ad525301dfeac3f98c87068d6211e6e02f6388bc1de4cfb2', // 0.3
 ];
 
 test('released layout steps never change; new ones come after them', () => {
